a76e4dc903bf380aa95aa8c39293dce6981e50b6
高橋　宗太 <souta_takahashi@012grp.co.jp>
2020-08-25 10:52:07 +0900

    [ts-migrate][src] Run TS Migrate
    Co-authored-by: ts-migrate <>

--- src/jsx/containers/Hoge.tsx
+++ src/jsx/containers/Hoge.tsx
@@ -1,4 +1,6 @@
+// @ts-expect-error ts-migrate(7016) FIXME: Try `npm install @types/react` if it exists or add... Remove this comment to see the full error message
 import React from 'react'
+// @ts-expect-error ts-migrate(7016) FIXME: Try `npm install @types/react-router-dom` if it ex... Remove this comment to see the full error message
 import { BrowserRouter as Router, useHistory } from 'react-router-dom'
 
 const Hoge = () => {
@@ -9,9 +11,13 @@ const Hoge = () => {
   }
 
   return (
+    // @ts-expect-error ts-migrate(7026) FIXME: JSX element implicitly has type 'any' because no i... Remove this comment to see the full error message
     <div>
+      {/* @ts-expect-error ts-migrate(7026) FIXME: JSX element implicitly has type 'any' because no i... Remove this comment to see the full error message */}
       <p>Hoge</p>
+      {/* @ts-expect-error ts-migrate(7026) FIXME: JSX element implicitly has type 'any' because no i... Remove this comment to see the full error message */}
       <button onClick={handleClick}>Top</button>
+    {/* @ts-expect-error ts-migrate(7026) FIXME: JSX element implicitly has type 'any' because no i... Remove this comment to see the full error message */}
     </div>
   )
 }
